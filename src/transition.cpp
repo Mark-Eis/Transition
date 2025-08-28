0c020b334a7c5cddd7c68ad0739de7bfbee1f244
Mark-Eis <mark@markeisler.com>
2025-08-28 13:13:10 +0100

    update transition.cpp; tweak documentation, issue #10

--- src/transition.cpp
+++ src/transition.cpp
@@ -296,9 +296,9 @@ inline IntegerVector prevres_intvec(DataFrame object, const char* subject, const
 //' the column named as specified by argument \code{timepoint} (see \emph{Note}).
 //'
 //' Test results should either be semi-quantitiative, formatted as an
-//' \code{\link[base:ordered]{ordered factor}}, or binary data formatted as an \code{\link{integer}}
-//' (or \code{\link{numeric}}) vector with values of either \code{1} or \code{0}, and included in
-//' \code{object} in the data frame column specified by argument \code{result} (see \emph{Note}).
+//' \code{\link[base:ordered]{ordered factor}} (see \emph{Note}), or binary data formatted as an
+//' \code{\link{integer}} (or \code{\link{numeric}}) vector with values of either \code{1} or \code{0},
+//' and included in \code{object} in the data frame column specified by argument \code{result}.
 //'
 //' Temporal transitions in the test \code{results} for each \code{subject} within the \code{object}
 //' \code{\link{data.frame}} are characterised using methods governed by options \code{cap} and
